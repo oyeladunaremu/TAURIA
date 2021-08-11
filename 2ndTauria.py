762bd318436d12bfe95c3bd29b0cb41f91e46d5e
Oyeladun Aremu <oyeladun.aremu@wakanow.com>
2021-08-11 15:50:27 +0100

    add Tauria

---
+++ 2ndTauria.py
@@ -0,0 +1,46 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import re
+import random
+import string
+
+
+def random_string(length):  
+    letters = string.ascii_lowercase # define the specific string  
+    # define the condition for random.sample() method  
+    result1 = ''.join((random.sample(letters, length)))   
+    # print(" Random generated string without repetition: ", result1)
+    return result1
+ab=random_string(5)
+print (ab)
+
+#opening the browser
+driver=webdriver.Chrome(executable_path=r"C:\\Users\\oyeladun.aremu\\Downloads\\chromedriver_win32 (2)\\chromedriver.exe")
+driver.maximize_window()
+
+driver.implicitly_wait(60)
+
+#setting up workspace
+driver.get("https://tauria.com/signin")
+driver.find_element_by_xpath("//button[contains(text(),'CREATE ONE')]").click()
+
+# personal details and email
+driver.find_element_by_xpath("//input[@id='name']").send_keys(random_string(5))
+driver.find_element_by_xpath("/html//div[@id='root']/div/div//form//button[.='NEXT']").click()
+driver.find_element_by_xpath("//input[@id='first']").send_keys("Oyes")
+driver.find_element_by_xpath("//input[@id='last']").send_keys("Cubes")
+driver.find_element_by_xpath("//input[@id='email']").send_keys(random_string(5)+"@mailinator.com")
+driver.find_element_by_xpath("/html//div[@id='root']/div/div//form//button[.='NEXT']").click()
+
+#setting password
+driver.find_element_by_xpath("//input[@id='password']").send_keys("MojTos2")
+driver.find_element_by_xpath("//input[@id='confirm']").send_keys("MojTos2")
+driver.find_element_by_xpath("/html//div[@id='root']/div/div//form//button[.='NEXT']").click()
+
+message=driver.find_element_by_xpath("//body/div[@class='o_web_client']/div[@class='ZebuMailOnboarding']//div[@class='OnboardingChoice-title']").text
+print(message)
+
+
+
+
---
+++ Tauria1.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import re
+
+
+
+
+def factorial_func(num):
+    factorial = 1
+    if num < 0:
+       print("Sorry, factorial does not exist for negative numbers")
+    elif num == 0:
+        print("The factorial of 0 is 1")
+    else:
+        for i in range(1,num + 1):
+            factorial = factorial*i
+        return factorial
+
+driver=webdriver.Chrome(executable_path=r"C:\\Users\\oyeladun.aremu\\Downloads\\chromedriver_win32 (2)\\chromedriver.exe")
+driver.maximize_window()
+driver.get("http://localhost:6464/")
+num_to_calc = 7
+driver.find_element_by_xpath("//input[@id='number']").send_keys(num_to_calc)
+driver.find_element_by_xpath("//button[@id='getFactorial']").click()
+time.sleep(3)
+
+result = driver.find_element_by_xpath("//p[@id='resultDiv']").text
+#Gettingthe index of amount int the string and cutting it off
+index = result.find(": ") + 2
+ui_amount = result[index:]
+# converting the amount string to integer
+ui_amount = int(ui_amount)
+#calculating the factorial
+actual_factorail = factorial_func(num_to_calc)
+
+#asserting that what was input on the UI returns correct result
+assert ui_amount == actual_factorail
+print("Amounts are equal.\nTest passed.") 
+ 
